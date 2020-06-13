f8449da3ac71964263ed31ba72da7a808223f624
levimartines <levi.mferreira@gmail.com>
2020-06-13 18:55:17 -0300

    Cadastro de imagens de usuario

--- stream_video.py
+++ stream_video.py
@@ -1,17 +1,16 @@
 import time
 import cv2
-from flask import Flask, render_template, Response
+import numpy as np
+from flask import Flask, render_template, Response, request
 
 detector_face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#recognizer = cv2.face.FisherFaceRecognizer_create()
-#recognizer.read("fisher_classifier.yml")
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("lbph_classifier.yml")
 width, height = 220, 220
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
 app = Flask(__name__)
-
+codigo: int = 0
 
 @app.route('/')
 def index():
@@ -21,6 +20,24 @@ def index():
 @app.route('/camera')
 def camera():
     return render_template('camera.html')
+
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+
+@app.route('/cadastro', methods=['POST'])
+def my_form_post():
+    text = request.form['codigo']
+    global codigo
+    codigo = int(text)
+    return render_template('cadastroCam.html')
+
+
+@app.route('/cadastro-camera')
+def cadastroCamera():
+    return render_template('cadastroCam.html')
 
 
 def gen():
@@ -55,3 +72,42 @@ def gen():
 def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def capture():
+    cap = cv2.VideoCapture(0)
+
+    larg, alt = 220, 220
+    amostra: int = 1
+    id = 1
+
+    while cap.isOpened():
+        ret, img = cap.read()
+        image_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        detected_faces = detector_face.detectMultiScale(image_grey, scaleFactor=1.5, minSize=(100, 100))
+        if ret:
+            for (x, y, l, a) in detected_faces:
+                cv2.rectangle(img, (x, y), (x + l, y + a), (0, 0, 255), 2)
+                global codigo
+
+                if np.average(image_grey) > 70:
+                    imagemface = cv2.resize(image_grey[y:y + a, x:x + l], (larg, alt))
+                    cv2.imwrite("faces/pessoa." + str(codigo) + "." + str(amostra) + ".jpg", imagemface) + amostra
+                    print("Foto capturada com sucesso - " + str(amostra))
+                    amostra += 1
+
+                    if amostra > 15:
+                        exit(0)
+
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+            time.sleep(0.1)
+        else:
+            break
+
+
+@app.route('/video_capture')
+def video_capture():
+    return Response(capture(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
